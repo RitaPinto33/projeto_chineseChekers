818dbd205d804ba41494257fde9d67d5d9fdcb95
RitaPinto33 <100215885+RitaPinto33@users.noreply.outlook.com>
2023-06-14 23:19:41 +0100

    Merge origin/main into main
    Conflicts:
    	README.md
    aass

--- README.md
+++
@@ -1,2 +0,0 @@
-# LDP_chineseCheckers
-trabalho de ldp
---
+++ git_chinesechekers/src/git_chinesechekers/tabuleiro.java
@@ -0,0 +1,15 @@
+package git_chinesechekers;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ritap
+ */
+public class tabuleiro {
+    
+}
