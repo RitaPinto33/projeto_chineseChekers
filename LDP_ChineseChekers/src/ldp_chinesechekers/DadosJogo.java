0f1fd302381bf0f43819f8ea90f6b0087a36f700
RitaPinto33 <100215885+RitaPinto33@users.noreply.outlook.com>
2023-06-14 23:47:31 +0100

    cores  que o jogador pode escolher

---
+++ LDP_ChineseChekers/src/ldp_chinesechekers/DadosJogo.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ldp_chinesechekers;
+
+/**
+ *
+ * @author ritap
+ */
+public class DadosJogo {
+    
+     Jogador jogador1, jogador2;
+    
+
+    // cores
+
+    /**
+     *Cor Vermelha
+     */
+    public static String Vermelho = "#ff1f1f";
+
+    /**
+     *Cor Laranja
+     */
+    public static String Laranja = "#ffc91f";
+
+    /**
+     *Cor Amarela
+     */
+    public static String Amarelo = "#eeff00";
+
+    /**
+     *Cor Verde
+     */
+    public static String Verde = "#009427";
+
+    /**
+     *Cor Branca
+     */
+    public static String Branco = "#ffffff";
+
+    /**
+     *Cor Azul Claro
+     */
+    public static String AzulClaro = "#99ccff";
+
+    /**
+     *Cor Azul Escuro
+     */
+    public static String AzulEscuro = "#6666ff";
+
+
+   
+
+
+
+}
