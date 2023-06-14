000a10588714366c3cc289b02c3ebc986464a807
RitaPinto33 <100215885+RitaPinto33@users.noreply.github.com>
2023-06-14 20:24:19 +0100

    jogo ini

--- git_chinesechekers/src/git_chinesechekers/Git_chinesechekers.java
+++ git_chinesechekers/src/git_chinesechekers/Git_chinesechekers.java
@@ -15,7 +15,7 @@ public class Git_chinesechekers {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // teste123
     }
     
 }
